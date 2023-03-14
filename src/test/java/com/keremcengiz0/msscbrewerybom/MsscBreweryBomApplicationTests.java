03c58cd8c8b59d53a2a85b2955841edbc781dabf
keremcengiz0 <keremcengiz0@hotmail.com>
2023-03-14 11:49:17 +0300

    initial commit

---
+++ src/test/java/com/keremcengiz0/msscbrewerybom/MsscBreweryBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keremcengiz0.msscbrewerybom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
